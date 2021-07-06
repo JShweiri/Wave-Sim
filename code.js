520fdd4ab8e462af30d4d80f2ff57988e3a4216e
JShweiri <wafflemanz3000@gmail.com>
2021-07-06 16:18:43 -0700

    changes

--- code.js
+++ code.js
@@ -5,6 +5,23 @@ canvas.height = 500;
 let ctx = canvas.getContext('2d');
 
 
+function waveFunc(x){
+
+    //return Math.sin(x)+Math.cos(x*x*x*x*x);
+
+    return Math.sin(x)+Math.cos(x*x-4*x);
+
+    return x - Math.pow(Math.E, 0.1*x)*Math.sin(x*x)+Math.pow(Math.E, 0.1*x)*Math.sin(5*x)*Math.sin(2*x);
+
+    return 0.8*x - Math.pow(Math.E, 0.1*x)*Math.sin(x*x/3)+4*Math.sin(5*x);
+
+    return 0.3*x - Math.sin(x+1)+5*Math.pow(Math.E, -x)*Math.sin(10*x)+Math.pow(Math.E, 0.14*x)*Math.sin(3*x);
+
+
+    return Math.pow(Math.E, 0.05*x)*Math.sin(x/5);
+}
+
+
 //final time
 let finalTime = 25;
 
@@ -45,17 +62,12 @@ let u = [];
 //initial conditions                                    SET FUNCTION HERE!!!!!!!!!!!!!
 u[0] = [];
 for (let i = 0; i <= nx; i++){
-    u[0][i] =   Math.sin(dx*i) + Math.sin(dx*i/2);
+    u[0][i] =   waveFunc(dx*i); // + Math.pow(Math.E, 0.05*dx*i)*Math.sin(dx*i/5);
 }
 
 //Set Global Boundry Conditions
 let LB = u[0][0];
 let RB = u[0][nx];
-
-//OLD
-// //first time step boundry conditions
-// u[0][0] = 0;
-// u[0][nx] = 0;
 
 
 //First Layer
@@ -105,17 +117,26 @@ function drawPoint(point){
 }
 
 let j = 0;
-function drawString(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+function drawString(fade = false){
+
+    if (fade){
     if(j<nt){
     for(let i = 0; i < nx; i++){
-        drawPoint({x:i*dx, y: MAX_DISTANCES.y/2 - u[j][i], color:'black'});
+        drawPoint({x:i*dx, y: MAX_DISTANCES.y/2 - u[j][i], color:'#00000010'});
     }
+}
+} else {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    if(j<nt){
+        for(let i = 0; i < nx; i++){
+            drawPoint({x:i*dx, y: MAX_DISTANCES.y/2 - u[j][i], color:'black'});
+        }
+}
 }
     j++;
 }
 
-setInterval(drawString, 1);
+setInterval(drawString, 5);
 
 
 
@@ -164,11 +185,16 @@ function submitted(){
 
     recalculate();
     j = 0;
-    setInterval(drawString, 1);
+    setInterval(drawString, 5);
 
 };
 
 function recalculate(){
+
+    C = smallc*dt/dx;
+
+    //C squared
+    C2 = C*C;
 
 //Number of time steps
 nt = Math.ceil(finalTime/dt);
@@ -181,7 +207,7 @@ nx = Math.ceil(XMAX/dx);
 //initial conditions                                                AND HERE!!!!!!!!!!!!!
 u[0] = [];
 for (let i = 0; i <= nx; i++){
-    u[0][i] =   Math.sin(dx*i) + Math.sin(dx*i/2);
+    u[0][i] =   waveFunc(dx*i);
 }
  LB = u[0][0];
  RB = u[0][nx];
